dc154299ea4cd5f9b1c2581eddd568ce2f31ed2f
dharshinis-it19 <dharshinis.it19@bitsathy.ac.in>
2019-11-01 11:51:55 +0530

    first commit

---
+++ prog47.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int l,b,a;
+ printf("enter the length of the rectangle");
+ scanf("%d",&l);
+ printf("enter the breadth of the rectangle");
+ scanf("%d",&b);
+ a=l*b;
+ printf("the area of the rectangle is %d units",a);
+ return 0;
+}
+
